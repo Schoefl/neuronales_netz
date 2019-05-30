ffe12ac812ba437ece133f273d86c82b868d894a
Schoefl <amanda.schoefl@student.tuwien.ac.at>
2019-05-30 14:19:38 +0200

    fit function for more than one x implemented
    ToDo:
    - training with real values
    - stocastic gradient descent

--- nn.py
+++ nn.py
@@ -2,21 +2,23 @@ import numpy as np
 from numpy.random import random
 
 class NeuronalNet:
-    def __init__(self, amount_input_neurons=3, amount_hidden_neurons=9, hidden_layers=1, amount_output_neurons=1):
+    def __init__(self, amount_input_neurons=3, amount_hidden_neurons=9, hidden_layers=1, amount_output_neurons=1, mini_batch_size=3):
         self.__ain = amount_input_neurons
         self.__ahn = amount_hidden_neurons
         self.__hl = hidden_layers
         self.__aon = amount_output_neurons
         self.__learning_rate = 0.001
+        self.__mbs = mini_batch_size
 
         ## use same notation as in lecture
-
         ## initialize weights 
         self.__w = [None]*(self.__hl+1)
-        self.__w[0] = random([self.__ahn, self.__ain]) # w for second layer
+        self.__w[0] = random([self.__ahn, self.__ain])               # w for second layer
         if self.__hl > 1:
-            pass # ToDo: hier noch was machen
-        self.__w[len(self.__w)-1] = random([self.__aon, self.__ahn])
+            for j in range(1,self.__hl):                             # w for hidden layers
+                self.__w[j] = random([self.__ahn, self.__ahn])
+        self.__w[len(self.__w)-1] = random([self.__aon, self.__ahn]) # w for last layer
+
 
         ## initialize bias
         self.__b = [None]*(self.__hl+1)
@@ -24,77 +26,46 @@ class NeuronalNet:
             self.__b[j] = random(self.__ahn)
         self.__b[len(self.__b)-1] = random(self.__aon)
 
-        ## initialize a
+        ## make sure self.__feedForward(x), self.__backPropagation(y) are not called in wrong context
+        self.__a = None
+        self.__z = None
+        self.__delta = None   
+
+    def _fit(self, x, y):
+        assert(x.shape[0]==self.__ain and y.shape[0]==self.__aon and x.shape[1]==y.shape[1] and x.shape[1]!=0), "invalid input in fit"
+
         self.__a = [None]*(self.__hl+2)
-
-        ## initialize z
         self.__z = [None]*(self.__hl+1)
-
-        ## initialize delta
         self.__delta = [None]*(self.__hl+1)
-            
-    def test(self):
+        delta_w = [None]*(self.__hl+1) # delta for w
+        delta_b = [None]*(self.__hl+1) # delta for b
 
-        x = [1,0,0.5]
-        y = [0]
+        for j in range(0, x.shape[1]):
+            self.__feedForward(x[:,j])
+            self.__backPropagation(y[:,j])
+            for i in range(0, len(self.__delta)):
+                if delta_w[i]==None:
+                    delta_w[i] = np.outer(self.__delta[i], np.matrix.transpose(self.__a[i]))
+                else:
+                    delta_w[i] += np.outer(self.__delta[i], np.matrix.transpose(self.__a[i]))
+                if delta_b[i]==None:
+                    delta_b[i] = self.__delta[i]
+                else:
+                    delta_b[i] += self.__delta[i]
 
-        count=0
-        while(count < 10000):
-            self.feedForward(x)
-            self.backPropagation(y)
-            self.gradientDescent()
-            if self.mse(self.__a[len(self.__a)-1], y) < 0.00001: break
-            count += 1
-            # print("ROUND", count)
-            # print("z \n", self.__z)
-            # print("a \n", self.__a)
-            # print("w \n", self.__w)
-            # print("b \n", self.__b)
-            # print("delta \n", self.__delta)
+        for i in range(0, len(self.__w)):
+            self.__w[i] = self.__w[i]-self.__learning_rate/x.shape[1]*delta_w[i]
+            self.__b[i] = self.__b[i]-self.__learning_rate/x.shape[1]*delta_b[i]
 
-        print("stopped at count", count)
-        print("output train: ", self.__a[len(self.__a)-1])
-        self.feedForward([0,1,0])
-        print("output test: ", self.__a[len(self.__a)-1])
+        ret_val = self.mse(self.__a[len(self.__a)-1], y)
+        ## make sure self.__feedForward(x), self.__backPropagation(y) are not called in wrong context
+        self.__a = None
+        self.__z = None
+        self.__delta = None        
+        return ret_val
 
-
-
-        # self.feedForward(x)
-        # self.backPropagation(y)
-        # self.gradientDescent()
-        # print("z \n", self.__z)
-        # print("a \n", self.__a)
-        # print("w \n", self.__w)
-        # print("b \n", self.__b)
-        # print("delta \n", self.__delta)
-
-        #print("self.__w \n", self.__w)
-
-    def mse(self, a, y):
-        """
-        input: a..output of net, y..value it should be
-        output: mean sqared error 
-        """
-        assert(len(a)==len(y)), "a and y have different lengths"
-        return np.square(np.array(a)-np.array(y)).mean()
-
-    def sigma(self, x, deriv=False):
-        ## activation function
-        x = np.array(x)
-        if deriv==False:
-            #assert((x>=0).all() and (x<=1).all()), "input must be in range [0,1]"
-            #return 1 / (1 + np.exp(-x)) # sigmoid
-            ## relu
-            ret_val = x
-            ret_val[ret_val<0]=0
-        else:
-            # relu
-            ret_val=np.zeros(len(x))
-            ret_val[x>0]=1
-        return np.array(ret_val)
-
-    def feedForward(self, x):
-        ## ToDo: effizienter gestalten
+    def __feedForward(self, x):
+        assert(self.__a!=None and self.__z!=None), "function should not be called here"
         a = self.__a 
         z = self.__z
         a[0] = np.array(x)
@@ -104,8 +75,10 @@ class NeuronalNet:
         self.__a = a
         self.__z = z
 
-    def backPropagation(self, y):
-        assert(len(y)==len(self.__a[len(self.__a)-1])), "len(y) incorrect in backPropagation"
+
+    def __backPropagation(self, y):
+        assert(self.__a!=None and self.__z!=None and self.__delta!=None), "function should not be called here"
+        assert(len(y)==len(self.__a[len(self.__a)-1])), "len(y) incorrect in __backPropagation"
         delta = self.__delta
         delta[len(delta)-1] = np.multiply((self.__a[len(self.__a)-1]-y), self.sigma(self.__z[len(self.__z)-1], deriv=True))
 
@@ -113,11 +86,38 @@ class NeuronalNet:
             delta[j] = np.multiply(np.matmul(np.matrix.transpose(self.__w[j+1]), delta[j+1]), self.sigma(self.__z[j], deriv=True))
         self.__delta = delta
 
-    def gradientDescent(self):
-        for j in range(0, len(self.__w)):
-            self.__w[j] = self.__w[j]-self.__learning_rate*np.outer(self.__delta[j], np.matrix.transpose(self.__a[j]))
-            self.__b[j] = self.__b[j]-self.__learning_rate*self.__delta[j]
-        
+
+    def test(self):
+        x = np.array([1,0,0.5]).reshape((3,1))
+        y = np.array([0]).reshape((1,1))
+
+        print(self._fit(x,y))
+
+
+    @staticmethod
+    def mse(a, y):
+        """
+        input: a..output of net, y..value it should be
+        output: mean sqared error 
+        """
+        assert(len(a)==len(y)), "a and y have different lengths"
+        return np.square(np.array(a)-np.array(y)).mean()
+
+    @staticmethod
+    def sigma(x, deriv=False):
+        ## activation function
+        x = np.array(x)
+        if deriv==False:
+            ## relu
+            ret_val = x
+            ret_val[ret_val<0]=0
+        else:
+            # relu
+            ret_val=np.zeros(len(x))
+            ret_val[x>0]=1
+        return np.array(ret_val)
+
+
 
 ### tests
 nn = NeuronalNet()
