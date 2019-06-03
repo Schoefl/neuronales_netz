87479439e5ffe32c6284f95b4aaafeb9dcef9650
Schoefl <amanda.schoefl@student.tuwien.ac.at>
2019-06-03 12:14:46 +0200

    there is something very wrong!

--- nn.py
+++ nn.py
@@ -7,29 +7,43 @@ class NeuronalNet:
         self.__ahn = amount_hidden_neurons
         self.__hl = hidden_layers
         self.__aon = amount_output_neurons
-        self.__learning_rate = 0.001
-        self.__mbs = mini_batch_size
+
+        self.learning_rate = 0.001
+        self.mbs = mini_batch_size
+        self.error_th = 0.001
+        random_multi_w = 0.0001
+        random_multi_b = 0.000000001
+        add = 1/2
 
         ## use same notation as in lecture
         ## initialize weights 
         self.__w = [None]*(self.__hl+1)
-        self.__w[0] = random([self.__ahn, self.__ain])               # w for second layer
+        self.__w[0] = (random([self.__ahn, self.__ain])-add)*random_multi_w            # w for second layer
         if self.__hl > 1:
             for j in range(1,self.__hl):                             # w for hidden layers
-                self.__w[j] = random([self.__ahn, self.__ahn])
-        self.__w[len(self.__w)-1] = random([self.__aon, self.__ahn]) # w for last layer
+                self.__w[j] = (random([self.__ahn, self.__ahn])-add)*random_multi_w
+        self.__w[len(self.__w)-1] = (random([self.__aon, self.__ahn])-add)*random_multi_w # w for last layer
 
 
         ## initialize bias
         self.__b = [None]*(self.__hl+1)
         for j in range(0,self.__hl):
-            self.__b[j] = random(self.__ahn)
-        self.__b[len(self.__b)-1] = random(self.__aon)
+            self.__b[j] = (random(self.__ahn)-add)*random_multi_b
+        self.__b[len(self.__b)-1] = (random(self.__aon)-add)*random_multi_b
 
         ## make sure self.__feedForward(x), self.__backPropagation(y) are not called in wrong context
         self.__a = None
         self.__z = None
         self.__delta = None   
+    
+    def train(self, x, y, max_steps=10000):
+        ## note that numpy arrays are passed by reference, so passing x,y as often as I do it here is not inefficient
+        assert(x.shape[0]==self.__ain and y.shape[0]==self.__aon and x.shape[1]==y.shape[1] and x.shape[1]!=0), "invalid input in fit"
+        step = 0
+        while self._fit(x,y):
+            if step>=max_steps: break
+            step += 1
+        print("stopped after {} steps".format(step))
 
     def _fit(self, x, y):
         assert(x.shape[0]==self.__ain and y.shape[0]==self.__aon and x.shape[1]==y.shape[1] and x.shape[1]!=0), "invalid input in fit"
@@ -39,25 +53,37 @@ class NeuronalNet:
         self.__delta = [None]*(self.__hl+1)
         delta_w = [None]*(self.__hl+1) # delta for w
         delta_b = [None]*(self.__hl+1) # delta for b
+        mean_mse = 0
+        ret_val = True
 
         for j in range(0, x.shape[1]):
+            self.__a = [None]*(self.__hl+2)
             self.__feedForward(x[:,j])
             self.__backPropagation(y[:,j])
+            mean_mse += self.mse(self.__a[len(self.__a)-1], y[:,j])
             for i in range(0, len(self.__delta)):
-                if delta_w[i]==None:
+                if type(delta_w[i])==type(None):
                     delta_w[i] = np.outer(self.__delta[i], np.matrix.transpose(self.__a[i]))
                 else:
                     delta_w[i] += np.outer(self.__delta[i], np.matrix.transpose(self.__a[i]))
-                if delta_b[i]==None:
+                if type(delta_b[i])==type(None):
                     delta_b[i] = self.__delta[i]
                 else:
                     delta_b[i] += self.__delta[i]
+            # print("y: ", y[:,j])
+            # print("output: ", self.__a[len(self.__a)-1])
+        print("mse", mean_mse/(j+1), "y: ", np.argmax(y[:,j])+1,"output: ", np.argmax(self.__a[len(self.__a)-1])+1)
+        mean_mse = mean_mse/x.shape[1]
+        # print("mse", mean_mse,"y: ", y[:,j])
+        # print("mse", mean_mse, "output: ", self.__a[len(self.__a)-1], np.argmax(self.__a[len(self.__a)-1])+1)
+        #print("mse", mean_mse)
+        if mean_mse > self.error_th:
+            for i in range(0, len(self.__w)):
+                self.__w[i] = self.__w[i]-self.learning_rate/x.shape[1]*delta_w[i]
+                self.__b[i] = self.__b[i]-self.learning_rate/x.shape[1]*delta_b[i]
+        else: 
+            ret_val=False
 
-        for i in range(0, len(self.__w)):
-            self.__w[i] = self.__w[i]-self.__learning_rate/x.shape[1]*delta_w[i]
-            self.__b[i] = self.__b[i]-self.__learning_rate/x.shape[1]*delta_b[i]
-
-        ret_val = self.mse(self.__a[len(self.__a)-1], y)
         ## make sure self.__feedForward(x), self.__backPropagation(y) are not called in wrong context
         self.__a = None
         self.__z = None
@@ -66,32 +92,44 @@ class NeuronalNet:
 
     def __feedForward(self, x):
         assert(self.__a!=None and self.__z!=None), "function should not be called here"
-        a = self.__a 
-        z = self.__z
-        a[0] = np.array(x)
+        ## assign new names to make clear which values are going to be changed, really this is just a reference
+        ## to the same variable, so if a gets changed, self.__a is changed as well
+        #a = self.__a 
+        #z = self.__z
+        self.__a[0] = np.array(x)
+        # print("self.__a[0].shape", self.__a[0].shape)
         for j in range(0,self.__hl+1):
-            z[j] = np.matmul(self.__w[j], a[j]) + self.__b[j]
-            a[j+1] = self.sigma(x=z[j])
-        self.__a = a
-        self.__z = z
+            # print("self.__w[{}].shape".format(j), self.__w[j].shape)
+            self.__z[j] = np.inner(self.__w[j],self.__a[j]) + self.__b[j]
+            self.__a[j+1] = self.sigma(x=self.__z[j], deriv=False)
+        #     print("self.__a[{}].shape".format(j+1), self.__a[j+1].shape)
+        # print("self.__z[0]", self.__z[0])
+        # print("self.__z[1]", self.__z[1])
+        # print("self.__a[1]", self.__a[1])
+        # print("self.__a[2]", self.__a[2])
 
+        # print(self.__w[j].shape, self.__a[j].shape)
+        # print("np.matmul(self.__w[j], self.__a[j])",np.outer(self.__w[j], self.__a[j]))
+        # print("self.__a[j-1]", self.__a[j-1])
+        # print("self.__w[j]", self.__w[j])
 
     def __backPropagation(self, y):
         assert(self.__a!=None and self.__z!=None and self.__delta!=None), "function should not be called here"
         assert(len(y)==len(self.__a[len(self.__a)-1])), "len(y) incorrect in __backPropagation"
-        delta = self.__delta
-        delta[len(delta)-1] = np.multiply((self.__a[len(self.__a)-1]-y), self.sigma(self.__z[len(self.__z)-1], deriv=True))
+        delta = self.__delta # create new reference to self.__delta same as in __feedForward
+        self.__delta[len(self.__delta)-1] = np.multiply((self.__a[len(self.__a)-1]-y), self.sigma(self.__z[len(self.__z)-1], deriv=True))
 
-        for j in range(len(delta)-2, -1, -1):
-            delta[j] = np.multiply(np.matmul(np.matrix.transpose(self.__w[j+1]), delta[j+1]), self.sigma(self.__z[j], deriv=True))
-        self.__delta = delta
+        for j in range(len(self.__delta)-2, -1, -1):
+            self.__delta[j] = np.multiply(np.matmul(np.matrix.transpose(self.__w[j+1]), self.__delta[j+1]), self.sigma(self.__z[j], deriv=True))
 
 
-    def test(self):
-        x = np.array([1,0,0.5]).reshape((3,1))
-        y = np.array([0]).reshape((1,1))
-
-        print(self._fit(x,y))
+    def test(self, x):
+        self.__a = [None]*(self.__hl+2)
+        self.__z = [None]*(self.__hl+1)
+        self.__feedForward(x[:,0])
+        #print("self.__a[len(self.__a)-1]", self.__a[len(self.__a)-1])
+        self.__feedForward(x[:,1])
+        #print("self.__a[len(self.__a)-1]", self.__a[len(self.__a)-1])
 
 
     @staticmethod
@@ -104,21 +142,29 @@ class NeuronalNet:
         return np.square(np.array(a)-np.array(y)).mean()
 
     @staticmethod
-    def sigma(x, deriv=False):
+    def sigma(x, deriv=False, func="sigmoid"):
         ## activation function
-        x = np.array(x)
-        if deriv==False:
-            ## relu
-            ret_val = x
-            ret_val[ret_val<0]=0
+        #x = np.array(x)
+        if func=="sigmoid":
+            if deriv==False:
+                ## relu
+                # ret_val = x
+                # ret_val[ret_val<0]=0
+                ret_val = 1/(1+np.exp(-x))
+                #print("ret_val", ret_val)
+            else:
+                ## relu
+                # ret_val=np.zeros(len(x))
+                # ret_val[x>0]=1
+                ret_val = NeuronalNet.sigma(x, deriv=False)*(1-NeuronalNet.sigma(x, deriv=False))
+        elif func=="relu":
+            if deriv==False:
+                ret_val = x
+                ret_val[ret_val<0]=0
+            else:
+                ret_val=np.zeros(len(x))
+                ret_val[x>0]=1
         else:
-            # relu
-            ret_val=np.zeros(len(x))
-            ret_val[x>0]=1
-        return np.array(ret_val)
-
-
-
-### tests
-nn = NeuronalNet()
-nn.test()
+            assert(False), "Invalide Function"
+        #ret_val = 1/(1+np.exp(-x))
+        return ret_val
---
+++ test_nn.py
@@ -0,0 +1,42 @@
+import scipy.io as spio
+import numpy as np
+
+from mat4py import loadmat
+from nn import NeuronalNet
+
+
+def convertYData(y):
+    y = np.array(y, dtype=int)
+    ret_val = np.array(np.zeros((10,len(y)), dtype=bool))
+    for j in range(0,len(y)):
+        ret_val[y[j], j] = True
+    return ret_val
+
+def convertXData(x):
+    max_val = 255
+    ret_val = np.matrix.transpose((np.asarray(mat['trainX'])/max_val))
+    return ret_val
+
+
+mat = loadmat('mnist.mat')
+# test = np.zeros((784,2))
+# test = np.matrix.transpose((np.asarray(mat['trainX'])/255))[:,0:1]
+# print(test[test!=0])
+# mat = spio.loadmat('mnist.mat', squeeze_me=False)#, mat_dtype="double")
+
+#trainX = np.matrix.transpose((np.asarray(mat['trainX'])/255))[:,1:4]
+trainX = convertXData(mat['trainX'])[:,0:100]
+trainY = convertYData(mat['trainY'])[:,0:100]
+
+# print(trainX.shape(), trainY.shape())
+# tmp1 = trainX[:,0]
+# tmp2 = trainX[:,2]
+# print(tmp1[tmp1!=0])
+# print(tmp2[tmp2!=0])
+print(all(trainX[:,0]==trainX[:,1]))
+# print(all(trainY[:,0]==trainY[:,2]))
+
+
+net = NeuronalNet(amount_input_neurons=trainX.shape[0], amount_hidden_neurons=round(trainX.shape[0]), amount_output_neurons=trainY.shape[0])
+net.train(trainX, trainY)
+#net.test(trainX)
